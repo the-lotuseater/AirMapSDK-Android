5b9c2d65c576c7cbc55d50773b4d96797a83216e
Vansh Gandhi <vansh@airmap.com>
2016-09-01 19:27:40 -0700

    Updated documentation

--- airmapsdk/src/main/java/com/airmap/airmapsdk/Networking/Services/AirMap.java
+++ airmapsdk/src/main/java/com/airmap/airmapsdk/Networking/Services/AirMap.java
@@ -454,10 +454,10 @@ public class AirMap {
      * @param endAfter        Search for flights that end after this time
      * @param endBefore       Search for flights that end before this time
      * @param country         Search for flights within this country (Length: 3, case insensitive)
-     * @param startsAfterNow  Search for flights starting after now
-     * @param startsBeforeNow Search for flights starting before now
-     * @param endsAfterNow    Search for flights ending after now
-     * @param endsBeforeNow   Search for flights ending before now
+     * @param startsAfterNow  Search for flights starting after now (the startsAfter Date will be ignored)
+     * @param startsBeforeNow Search for flights starting before now (the startsBefore Date will be ignored)
+     * @param endsAfterNow    Search for flights ending after now (the endsAfter Date will be ignored)
+     * @param endsBeforeNow   Search for flights ending before now (the endsBefore Date will be ignored)
      * @param city            Search for flights within this city
      * @param state           Search for flights within this state
      * @param enhanced        Returns enhanced flight, pilot, and aircraft information
