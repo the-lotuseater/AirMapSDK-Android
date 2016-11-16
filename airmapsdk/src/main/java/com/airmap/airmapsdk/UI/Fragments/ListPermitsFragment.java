40a790723a7ca64e9535548ed79cdfd1a863d7dd
collin <collin@airmap.com>
2016-11-16 11:56:11 -0800

    added FAQ menu item to permits screen with link to faq page

--- airmapsdk/src/main/java/com/airmap/airmapsdk/UI/Fragments/ListPermitsFragment.java
+++ airmapsdk/src/main/java/com/airmap/airmapsdk/UI/Fragments/ListPermitsFragment.java
@@ -1,10 +1,14 @@
 package com.airmap.airmapsdk.ui.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -14,7 +18,9 @@ import com.airmap.airmapsdk.models.permits.AirMapAvailablePermit;
 import com.airmap.airmapsdk.models.permits.AirMapPilotPermit;
 import com.airmap.airmapsdk.models.status.AirMapStatusPermits;
 import com.airmap.airmapsdk.R;
+import com.airmap.airmapsdk.ui.activities.WebActivity;
 import com.airmap.airmapsdk.ui.adapters.PermitsAdapter;
+import com.airmap.airmapsdk.util.Constants;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -29,11 +35,16 @@ public class ListPermitsFragment extends Fragment {
     private Button nextButton;
 
     public ListPermitsFragment() {
-        // Required empty public constructor
     }
 
     public static ListPermitsFragment newInstance() {
         return new ListPermitsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -117,6 +128,25 @@ public class ListPermitsFragment extends Fragment {
     public void onResume() {
         super.onResume();
         setupRecyclerView();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        super.onCreateOptionsMenu(menu, menuInflater);
+        menuInflater.inflate(R.menu.menu_permits, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.permit_faq) {
+            Intent intent = new Intent(getActivity(), WebActivity.class);
+            intent.putExtra(Intent.EXTRA_TITLE, getString(R.string.faq));
+            intent.putExtra(Constants.URL_EXTRA, Constants.FAQ_PERMITS_URL);
+            startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(menuItem);
+        }
     }
 
     public interface OnFragmentInteractionListener {
--- airmapsdk/src/main/java/com/airmap/airmapsdk/util/Constants.java
+++ airmapsdk/src/main/java/com/airmap/airmapsdk/util/Constants.java
@@ -9,6 +9,9 @@ public class Constants {
     public static final String STATUS_PERMIT_EXTRA = "status_permit_extra";
     public static final String PERMIT_WALLET_EXTRA = "permit_wallet_extra";
     public static final String AVAILABLE_PERMIT_EXTRA = "available_permit_extra";
+    public static final String URL_EXTRA = "url_extra";
+
+    public static final String FAQ_PERMITS_URL = "https://cdn.airmap.io/static/webviews/faq.html#permit-what";
 
     public static final int CUSTOM_PROPERTIES_REQUEST_CODE = 112;
 
