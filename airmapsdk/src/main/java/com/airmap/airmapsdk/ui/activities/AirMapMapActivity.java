912319e76608e6ebe244850c862d891f36fb7b6a
cvance <collin@airmap.com>
2017-05-18 11:32:37 -0700

    moved mapactivity interface to sdk

---
+++ airmapsdk/src/main/java/com/airmap/airmapsdk/ui/activities/AirMapMapActivity.java
@@ -0,0 +1,27 @@
+package com.airmap.airmapsdk.ui.activities;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+
+import com.airmap.airmapsdk.models.Coordinate;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+
+import java.util.List;
+
+/**
+ * Created by collin@airmap.com on 5/15/17.
+ */
+
+public abstract class AirMapMapActivity extends AppCompatActivity {
+
+    public abstract MapboxMap getMap();
+
+    public abstract MapView getMapView();
+
+    public abstract void createFlight(final Coordinate coordinate);
+
+    public abstract void removeMapLayers(String sourceId, List<String> layerIds);
+
+    public abstract void addMapLayers(String sourceId, List<String> layerIds);
+}
