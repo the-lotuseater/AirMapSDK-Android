4dce921f60febc9b85a69959a36fa12aeeb0de9a
cvance <collin@airmap.com>
2017-06-29 13:57:31 -0700

    multidexed sample app

--- sample/src/main/java/com/airmap/airmapsdktest/MyApplication.java
+++ sample/src/main/java/com/airmap/airmapsdktest/MyApplication.java
@@ -1,9 +1,10 @@
 package com.airmap.airmapsdktest;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.airmap.airmapsdk.networking.services.AirMap;
-import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.Mapbox;
 
 import static com.airmap.airmapsdk.util.Utils.getMapboxApiKey;
 
@@ -11,12 +12,12 @@ import static com.airmap.airmapsdk.util.Utils.getMapboxApiKey;
  * Created by Vansh Gandhi on 8/12/16.
  * Copyright © 2016 AirMap, Inc. All rights reserved.
  */
-public class MyApplication extends Application {
+public class MyApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
         AirMap.init(this);
         AirMap.enableLogging(true);
-        MapboxAccountManager.start(this, getMapboxApiKey());
+        Mapbox.getInstance(this, getMapboxApiKey());
     }
 }
