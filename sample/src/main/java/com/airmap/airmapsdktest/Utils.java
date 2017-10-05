6d8af9c41a8332571cf7b659a13dc0f8575a38cf
cvance <collin@airmap.com>
2017-10-05 11:39:14 -0700

    Add flight plan demo

---
+++ sample/src/main/java/com/airmap/airmapsdktest/Utils.java
@@ -0,0 +1,90 @@
+package com.airmap.airmapsdktest;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
+import android.support.graphics.drawable.VectorDrawableCompat;
+import android.support.v4.content.ContextCompat;
+
+import com.airmap.airmapsdk.models.Coordinate;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+
+/**
+ * Created by collin@airmap.com on 10/4/17.
+ */
+
+public class Utils extends com.airmap.airmapsdk.util.Utils {
+
+    private static final int[] compassDirections = new int[]{
+            com.airmap.airmapsdk.R.string.cardinal_direction_n, com.airmap.airmapsdk.R.string.cardinal_direction_nnne, com.airmap.airmapsdk.R.string.cardinal_direction_ne, com.airmap.airmapsdk.R.string.cardinal_direction_ene,
+            com.airmap.airmapsdk.R.string.cardinal_direction_e, com.airmap.airmapsdk.R.string.cardinal_direction_ese, com.airmap.airmapsdk.R.string.cardinal_direction_se, com.airmap.airmapsdk.R.string.cardinal_direction_sse,
+            com.airmap.airmapsdk.R.string.cardinal_direction_s, com.airmap.airmapsdk.R.string.cardinal_direction_ssw, com.airmap.airmapsdk.R.string.cardinal_direction_sw, com.airmap.airmapsdk.R.string.cardinal_direction_wsw,
+            com.airmap.airmapsdk.R.string.cardinal_direction_w, com.airmap.airmapsdk.R.string.cardinal_direction_wnw, com.airmap.airmapsdk.R.string.cardinal_direction_nw, com.airmap.airmapsdk.R.string.cardinal_direction_nnw};
+
+
+    public static LatLng getLatLngFromCoordinate(Coordinate coordinate) {
+        if (coordinate != null) {
+            return new LatLng(coordinate.getLatitude(), coordinate.getLongitude());
+        }
+        return null;
+    }
+
+    public static String directionFromBearing(Context context, double bearing) {
+        int index = (int) ((bearing / 22.5) + 0.5) % 16;
+        return context.getResources().getString(compassDirections[index]);
+
+    }
+
+    public static String minutesToMinSec(Context context, double minutes) {
+        int min = (int) minutes;
+        int sec = (int) ((minutes - min) * 60);
+        return context.getString(R.string.minutes_seconds, min, sec);
+    }
+
+    public static double ktsToKmh(double kts) {
+        return kts * 1.852;
+    }
+
+    public static double ktsToMph(double kts) {
+        return kts * 1.151;
+    }
+
+    public static Bitmap getBitmap(Context context, @DrawableRes int drawableResId) {
+        Drawable drawable = ContextCompat.getDrawable(context, drawableResId);
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        } else if (drawable instanceof VectorDrawableCompat) {
+            return getBitmap((VectorDrawableCompat) drawable);
+        } else if (drawable instanceof VectorDrawable) {
+            return getBitmap((VectorDrawable) drawable);
+        } else {
+            throw new IllegalArgumentException("Unsupported drawable type");
+        }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private static Bitmap getBitmap(VectorDrawable vectorDrawable) {
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        vectorDrawable.draw(canvas);
+        return bitmap;
+    }
+
+    private static Bitmap getBitmap(VectorDrawableCompat vectorDrawable) {
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        vectorDrawable.draw(canvas);
+        return bitmap;
+    }
+}
---
+++ sample/src/main/java/com/airmap/airmapsdktest/activities/FlightPlanDemoActivity.java
@@ -0,0 +1,215 @@
+package com.airmap.airmapsdktest.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.airmap.airmapsdk.AirMapException;
+import com.airmap.airmapsdk.AirMapLog;
+import com.airmap.airmapsdk.models.Coordinate;
+import com.airmap.airmapsdk.models.flight.AirMapFlightFeature;
+import com.airmap.airmapsdk.models.flight.AirMapFlightPlan;
+import com.airmap.airmapsdk.models.rules.AirMapRule;
+import com.airmap.airmapsdk.models.rules.AirMapRuleset;
+import com.airmap.airmapsdk.models.shapes.AirMapGeometry;
+import com.airmap.airmapsdk.models.shapes.AirMapPolygon;
+import com.airmap.airmapsdk.networking.callbacks.AirMapCallback;
+import com.airmap.airmapsdk.networking.services.AirMap;
+import com.airmap.airmapsdk.ui.views.RulesetsEvaluator;
+import com.airmap.airmapsdktest.R;
+import com.airmap.airmapsdktest.ui.FlightPlanDetailsAdapter;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by collin@airmap.com on 9/27/17.
+ */
+
+public class FlightPlanDemoActivity extends AppCompatActivity {
+
+    private static final String TAG = "AnonymousLoginActivity";
+
+    private Toolbar toolbar;
+    private EditText nameEditText;
+    private TextView startTimeTextView;
+    private TextView endTimeTextView;
+    private TextView altitudeTextView;
+    private SeekBar altitudeSeekBar;
+    private RecyclerView flightFeaturesRecyclerView;
+    private View loadingView;
+
+    private AirMapFlightPlan flightPlan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_plan);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(getTitle());
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nameEditText = findViewById(R.id.name_edit_text);
+        startTimeTextView = findViewById(R.id.start_time_value);
+        endTimeTextView = findViewById(R.id.end_time_value);
+        altitudeTextView = findViewById(R.id.altitude_value);
+        altitudeSeekBar = findViewById(R.id.altitude_seekbar);
+        altitudeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int altitude = 10 + 10 * progress;
+                altitudeTextView.setText(altitude + " meters");
+                flightPlan.setMaxAltitude(altitude);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        flightFeaturesRecyclerView = findViewById(R.id.flight_features_recycler_view);
+        loadingView = findViewById(R.id.loading_view);
+
+        // create polygon from coordinates
+        List<Coordinate> coordinates = new ArrayList<>();
+        coordinates.add(new Coordinate(34.02440874647921, -118.49167761708696));
+        coordinates.add(new Coordinate(34.020040687842254, -118.4968401460024));
+        coordinates.add(new Coordinate(34.01648293903452, -118.4923151205652));
+        coordinates.add(new Coordinate(34.02080536486173, -118.48725884231055));
+        coordinates.add(new Coordinate(34.02440874647921, -118.49167761708696));
+
+        AirMapPolygon polygon = new AirMapPolygon();
+        polygon.setCoordinates(coordinates);
+
+        final JSONObject geometryJSON = AirMapGeometry.getGeoJSONFromGeometry(polygon);
+        // or use raw json
+//        {
+//            "type": "Polygon",
+//            "coordinates": [
+//                [
+//                    [-118.49167761708696, 34.02440874647921],
+//                    [-118.4968401460024, 34.020040687842254],
+//                    [-118.4923151205652, 34.01648293903452],
+//                    [-118.48725884231055, 34.02080536486173],
+//                    [-118.49167761708696, 34.02440874647921]
+//                ]
+//            ]
+//        }
+
+        // get rulesets (from jurisdictions) from geometry
+        AirMap.getRulesets(geometryJSON, new AirMapCallback<List<AirMapRuleset>>() {
+            @Override
+            protected void onSuccess(List<AirMapRuleset> availableRulesets) {
+                if (!availableRulesets.isEmpty()) {
+                    // Calculate selected rulesets based off preferred & unpreferred rulesets
+                    // If no preferred/unpreferred, defaults are selected
+                    List<AirMapRuleset> selectedRulesets = RulesetsEvaluator.computeSelectedRulesets(availableRulesets, new HashSet<String>(), new HashSet<String>());
+
+                    createFlightPlan(geometryJSON, 100, null, selectedRulesets);
+
+                    loadingView.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            protected void onError(AirMapException e) {
+                AirMapLog.e(TAG, "Unable to get rulesets from geometry: " + geometryJSON, e);
+            }
+        });
+    }
+
+    public void createFlightPlan(JSONObject geometry, float buffer, Coordinate takeoff, List<AirMapRuleset> selectedRulesets) {
+        flightPlan = new AirMapFlightPlan();
+
+        final List<String> selectedRulesetIds = new ArrayList<>();
+        final List<AirMapFlightFeature> flightFeatures = new ArrayList<>();
+
+        Map<AirMapFlightFeature, List<AirMapRule>> featuresMap = new HashMap<>();
+        for (AirMapRuleset ruleset : selectedRulesets) {
+            selectedRulesetIds.add(ruleset.getId());
+
+            if (ruleset.getRules() == null) {
+                continue;
+            }
+
+            for (AirMapRule rule : ruleset.getRules()) {
+                if (rule.getFlightFeatures() != null && !rule.getFlightFeatures().isEmpty()) {
+                    flightFeatures.addAll(rule.getFlightFeatures());
+
+                    for (AirMapFlightFeature flightFeature : rule.getFlightFeatures()) {
+                        AirMapLog.e(TAG, "Flight feature: " + flightFeature.getFlightFeature());
+
+                        List<AirMapRule> rules = new ArrayList<>();
+                        if (featuresMap.containsKey(flightFeature)) {
+                            rules = featuresMap.get(flightFeature);
+                        }
+                        rules.add(rule);
+
+                        featuresMap.put(flightFeature, rules);
+                    }
+                }
+            }
+        }
+
+        // sets required params
+        flightPlan = new AirMapFlightPlan();
+        flightPlan.setPilotId(AirMap.getUserId());
+        flightPlan.setGeometry(geometry.toString());
+        flightPlan.setBuffer(buffer);
+        flightPlan.setTakeoffCoordinate(takeoff);
+        flightPlan.setRulesetIds(selectedRulesetIds);
+
+        // default max alt - 100m
+        flightPlan.setMaxAltitude(100);
+        altitudeTextView.setText("100m");
+
+        // default start & end time - now to 4 hours from now
+        long duration = 4 * 60 * 60 * 1000;
+        flightPlan.setDurationInMillis(duration);
+        flightPlan.setStartsAt(new Date());
+        flightPlan.setEndsAt(new Date(System.currentTimeMillis() + duration));
+
+        SimpleDateFormat sdf = new SimpleDateFormat("h:mm a");
+        startTimeTextView.setText(sdf.format(flightPlan.getStartsAt()));
+        endTimeTextView.setText(sdf.format(flightPlan.getEndsAt()));
+
+
+        FlightPlanDetailsAdapter detailsAdapter = new FlightPlanDetailsAdapter(this, flightPlan, featuresMap, null, new FlightPlanDetailsAdapter.FlightPlanChangeListener() {
+            @Override
+            public void onFlightPlanChanged() {
+
+            }
+
+            @Override
+            public void onFlightFeatureRemoved(String flightFeature) {
+
+            }
+
+            @Override
+            public void onFlightPlanSave() {
+
+            }
+        });
+        flightFeaturesRecyclerView.setAdapter(detailsAdapter);
+    }
+}
