6a455b89b8fc36404a249824bc34ed9d9b6d6940
cvance <collin@airmap.com>
2017-10-12 14:14:21 -0700

    Add optional flightPlanId to flight object

--- airmapsdk/src/main/java/com/airmap/airmapsdk/util/AirMapConstants.java
+++ airmapsdk/src/main/java/com/airmap/airmapsdk/util/AirMapConstants.java
@@ -11,6 +11,8 @@ public class AirMapConstants {
     public static final String PILOT_EXTRA = "pilot_extra";
     public static final String FLIGHT_PLAN_ID_EXTRA = "flight_plan_id_extra";
     public static final String KEY_VALUE_EXTRAS = "key_value_extras";
+    public static final String START_DATE_EXTRA = "start_date_extra";
+    public static final String END_DATE_EXTRA = "end_date_extra";
 
     public static final String INFO_URL = "https://cdn.airmap.io/static/webviews/faq.html#let-others-know";
 
@@ -18,6 +20,8 @@ public class AirMapConstants {
     public static final String IMPERIAL_SYSTEM = "imperial_system";
     public static final String METRIC_SYSTEM = "metric_system";
 
+
+    public static final int CUSTOM_PROPERTIES_REQUEST_CODE = 112;
     public static final int CREATE_AIRCRAFT_REQUEST_CODE = 2494;
     public static final int EDIT_PROFILE_REQUEST_CODE = 6211;
 }
