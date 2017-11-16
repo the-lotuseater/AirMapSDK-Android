714f497dc855f01b05fe4627a8d092b6b46e24e8
cvance <collin@airmap.com>
2017-11-16 15:57:56 -0800

    Handle data driven fill-layers and line-layers

--- airmapsdk/src/main/java/com/airmap/airmapsdk/models/map/AirMapFillLayerStyle.java
+++ airmapsdk/src/main/java/com/airmap/airmapsdk/models/map/AirMapFillLayerStyle.java
@@ -1,7 +1,9 @@
 package com.airmap.airmapsdk.models.map;
 
+import android.text.TextUtils;
 import android.util.Log;
 
+import com.mapbox.mapboxsdk.style.functions.Function;
 import com.mapbox.mapboxsdk.style.layers.FillLayer;
 import com.mapbox.mapboxsdk.style.layers.Layer;
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
@@ -16,6 +18,8 @@ public class AirMapFillLayerStyle extends AirMapLayerStyle {
 
     public final float fillOpacity;
     public final String fillColor;
+    public final Function fillColorFunction;
+    public final String fillOutlineColor;
 
     protected AirMapFillLayerStyle(JSONObject json) {
         super(json);
@@ -23,10 +27,22 @@ public class AirMapFillLayerStyle extends AirMapLayerStyle {
         JSONObject paintJson = json.optJSONObject("paint");
         if (paintJson != null) {
             fillOpacity = (float) paintJson.optDouble("fill-opacity");
-            fillColor = optString(paintJson, "fill-color");
+
+            JSONObject fillColorJson = paintJson.optJSONObject("fill-color");
+            if (fillColorJson != null) {
+                fillColorFunction = getFillColorFunction(fillColorJson);
+                fillColor = "#000000";
+            } else {
+                fillColor = optString(paintJson, "fill-color");
+                fillColorFunction = null;
+            }
+
+            fillOutlineColor = optString(paintJson, "fill-outline-color");
         } else {
             fillOpacity = 0;
             fillColor = "#000000";
+            fillColorFunction = null;
+            fillOutlineColor = "#000000";
         }
     }
 
@@ -39,7 +55,16 @@ public class AirMapFillLayerStyle extends AirMapLayerStyle {
         FillLayer fillLayer = new FillLayer(id + "|" + sourceId + "|new", sourceId);
         fillLayer.setSourceLayer(sourceId + "_" + sourceLayer);
         fillLayer.setProperties(PropertyFactory.fillOpacity(fillOpacity));
-        fillLayer.setProperties(PropertyFactory.fillColor(fillColor));
+
+        if (!TextUtils.isEmpty(fillOutlineColor)) {
+            fillLayer.setProperties(PropertyFactory.fillOutlineColor(fillOutlineColor));
+        }
+
+        if (fillColorFunction != null) {
+            fillLayer.setProperties(PropertyFactory.fillColor(fillColorFunction));
+        } else {
+            fillLayer.setProperties(PropertyFactory.fillColor(fillColor));
+        }
 
         if (filter != null) {
             fillLayer.setFilter(filter);
