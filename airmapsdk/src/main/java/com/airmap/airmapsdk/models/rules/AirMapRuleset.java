86ba8931ae1a3bb5fe57a9f27ec6cc9445fa7d45
cvance <collin@airmap.com>
2017-12-15 18:51:44 -0800

    Fix AirMapRuleset serialization crash

--- airmapsdk/src/main/java/com/airmap/airmapsdk/models/rules/AirMapRuleset.java
+++ airmapsdk/src/main/java/com/airmap/airmapsdk/models/rules/AirMapRuleset.java
@@ -227,6 +227,9 @@ public class AirMapRuleset implements Serializable, AirMapBaseModel, Comparable 
 
     @Override
     public int hashCode() {
+        if (id == null) {
+            return 0;
+        }
         return id.hashCode();
     }
 }
